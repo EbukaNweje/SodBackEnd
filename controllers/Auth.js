cbb7f768587a2dd83f06664ae152c7b770d301e7
EbukaNweje <nwejeebuka@gmail.com>
2025-02-07 06:00:51 -0800

    F

--- controllers/Auth.js
+++ controllers/Auth.js
@@ -5,11 +5,18 @@ const {validationResult } = require('express-validator');
 // const createError = require("../utilities/error");
 
 
+
 exports.register = async (req, res, next)=>{
     try{    
         const { username, fullName, email, password } = req.body;
+        console.log(req.body);
+        
         const hashedPassword = await bcrypt.hash(password, 10);
-        const user = new User({ username, fullName, email, password: hashedPassword });
+        console.log(hashedPassword);
+
+        
+        const user = new User({ username, fullName, email, password });
+        console.log(user);
         await user.save();
 
         res.status(201).json({
@@ -26,9 +33,12 @@ exports.register = async (req, res, next)=>{
 exports.login = async (req, res, next)=>{
     try{    
         const { email, password } = req.body;
-        const user = await User.findOne({ email });
+        console.log(req.body);
+        const user = await User.findOne({ email:email });
+        
         if (!user) return res.status(404).send('User not found');
         const validPassword = await bcrypt.compare(password, user.password);
+        console.log(validPassword);
         if (!validPassword) return res.status(400).send('Invalid password');
         const token = jwt.sign({ _id: user._id }, 'secret');
         res.header('Authorization', token).send('Logged in');
